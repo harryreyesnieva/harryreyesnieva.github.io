abc1736252836de304ad9ccf29936e202cf87695
Harry Reyes Nieva <harryreyesnieva@gmail.com>
2025-09-01 21:18:22 -0400

    version 2.0

--- fetch_scholar_metrics.py
+++ fetch_scholar_metrics.py
@@ -1,22 +1,37 @@
-# fetch_scholar_metrics.py
-import yaml
+# # fetch_scholar_metrics.py
+# import yaml
+# from scholarly import scholarly
+#
+# # Your Google Scholar user ID
+# SCHOLAR_ID = "L58_1hAAAAAJ"
+#
+# # Fetch profile
+# profile = scholarly.search_author_id(SCHOLAR_ID)
+# profile = scholarly.fill(profile, sections=['indices'])
+#
+# # Extract metrics
+# metrics = {
+#     'citations': profile.get('citedby', 0),
+#     'h_index': profile.get('hindex', 0)
+# }
+#
+# # Write to Jekyll _data folder
+# with open('_data/scholar_metrics.yml', 'w') as f:
+#     yaml.dump(metrics, f)
+#
+# print("Google Scholar metrics updated:", metrics)
+
 from scholarly import scholarly
+import yaml
 
-# Your Google Scholar user ID
-SCHOLAR_ID = "L58_1hAAAAAJ"
+author = scholarly.search_author_id('L58_1hAAAAAJ')
+author_data = scholarly.fill(author)
 
-# Fetch profile
-profile = scholarly.search_author_id(SCHOLAR_ID)
-profile = scholarly.fill(profile, sections=['indices'])
-
-# Extract metrics
 metrics = {
-    'citations': profile.get('citedby', 0),
-    'h_index': profile.get('hindex', 0)
+    "citations": author_data['citedby'],
+    "h_index": author_data['hindex'],
+    "i10_index": author_data['i10index']
 }
 
-# Write to Jekyll _data folder
-with open('_data/scholar_metrics.yml', 'w') as f:
+with open("_data/scholar_metrics.yml", "w") as f:
     yaml.dump(metrics, f)
-
-print("Google Scholar metrics updated:", metrics)
